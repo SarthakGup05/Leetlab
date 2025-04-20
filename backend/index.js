eb09f3cc6e4e53c01986cdeee9296506c1d92dcb
SarthakGup05 <rythmgupta@outlook.com>
2025-04-20 15:04:54 +0530

    feat: add Prisma setup with schema and dependencies

--- backend/index.js
+++ backend/index.js
@@ -4,11 +4,9 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 
-
-
-
-
 const app = express();
+
+
 
 app.listen (process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
